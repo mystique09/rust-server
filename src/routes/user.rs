f537591f376dfe3e8171096588fb8dbb997f82cc
mystique09 <knightkrtpto7@gmail.com>
2021-12-04 00:35:05 +0800

    add user json response

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,8 +1,47 @@
 use rocket::*;
+use rocket::{
+  request::Request,
+  response::{self, Response, Responder},
+  serde::json::Json
+};
+use crate::serde::{
+  Serialize,
+  Deserialize
+};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Data {
+  pub status: String,
+  pub message: String
+}
+
+impl<'r> Responder<'r, 'static> for Data {
+  fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
+    Response::build()
+    //.raw_header("X-Data-Status", self.status)
+    .ok()
+  }
+}
+
+impl Data {
+  pub fn ok(message: &str) -> Self {
+    Data {
+      status: "Success".to_string(),
+      message: message.to_string(),
+    }
+  }
+  
+  pub fn err(message: &str) -> Self {
+    Data {
+      status: "Success".to_string(),
+      message: message.to_string(),
+    }
+  }
+}
 
 #[get("/")]
-pub fn index() -> &'static str {
-  "Welcome to the User route!"
+pub fn index() -> Json<Data> {
+  Json(Data::ok("Welcome to the User route!"))
 }
 
 #[get("/all")]
