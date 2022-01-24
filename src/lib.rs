6f4f3ad76ce22bab5556f2069bc8bec215f7815b
mystique09 <knightkrypto7@gmail.com>
2022-01-25 01:06:31 +0800

    comment PgConnection import

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-use diesel::pg::PgConnection;
+//use diesel::pg::PgConnection;
 use rocket::*;
-mod models;
-use models::*;
 pub mod routes;
 
 use routes::{index_route, message as message_route, user as user_route};
